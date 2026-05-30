aa7a156573e4743bbd74f5e574271c81abfbb6de
Javier Borjas <cristman11@gmail.com>
2026-05-30 19:38:09 -0300

    Permitiendo a usuarios verificados acceder al Dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.auth')
+
+@section('title')
+    Administra tus Presupuestos
+@endsection
+
+@section('auth-contents')
+    <h1 class="text-4xl">Administra tus Presupuestos</h1>
+@endsection
